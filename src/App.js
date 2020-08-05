f0eb00c82645c2596d40a36d22bf8af0547e87a5
Tiago Vilela <tiagovla@gmail.com>
2020-08-05 01:36:08 -0300

    updating the app with a shop page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component'
-
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
       <Switch>
         <Route path='/shop' component={ShopPage}></Route>
         <Route path='/' component={HomePage}></Route>
+        <Route path='/signin' component={SignInAndSignUp}></Route>
       </Switch> 
     </div>
   );
