6e814b851e21ae623b48b876e1c473c2d71ab610
Tiago Vilela <tiagovla@gmail.com>
2020-08-07 18:18:06 -0300

    added redux to user auth

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 import './header.styles.scss';
 import { ReactComponent as Logo} from '../../assets/crown.svg' 
 import {auth} from '../../firebase/firebase.utils'
@@ -20,4 +22,9 @@ const Header = ({currentUser}) => (
     </div>
 )
 
-export default Header;
+
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
